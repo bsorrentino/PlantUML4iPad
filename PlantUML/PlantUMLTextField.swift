a6e9c2f038e8478891fa4af9d2cbacbbcb3a5899
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-09-07 17:43:11 +0200

    feat: integrate use case to show a custom keyboard
    #1

---
+++ PlantUML/PlantUMLTextField.swift
@@ -0,0 +1,20 @@
+//
+//  PlantUMLTextField.swift
+//  PlantUML
+//
+//  Created by Bartolomeo Sorrentino on 06/09/22.
+//
+
+import SwiftUI
+
+struct PlantUMLTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlantUMLTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        PlantUMLTextField()
+    }
+}
