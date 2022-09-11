961a329e9243e0f16fc04abdc68e9ac82c822770
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-09-11 13:20:31 +0200

    add support for images within keyboard

--- PlantUMLKeyboard/Sources/PlantUMLKeyboard/PlantUMLKeyboard.swift
+++ PlantUMLKeyboard/Sources/PlantUMLKeyboard/PlantUMLKeyboard.swift
@@ -51,16 +51,80 @@ public func getRootViewController() -> UIViewController? {
     getFirstWindow()?.rootViewController
 }
 
-fileprivate var plantUMLSymbols = [
-    ["actor",
-    "boundary",
-    "control",
-    "entity",
-    "database",
-    "collections",
-    "queue"],
+fileprivate var plantUMLSymbols:[[String]] = [
+    [
+        "title",
+        "header",
+        "footer",
+        "autonumber"
+    ],
+    [
+        "participant",
+        "actor",
+        "boundary",
+        "control",
+        "entity",
+        "database",
+        "collections",
+        "queue"
+    ],
+    
+    [
+        "->x",
+        "->",
+        "->>",
+        "-\\\\",
+        "\\\\-",
+        "//--",
+        "->o",
+        "o\\\\--",
+        "<->",
+        "<->o"
+    ],
+    
+    [
+        "[#red]",
+        "note",
+        "end note"
+    ]
 
 ]
+
+fileprivate var plantUMLImages:[[UIImage?]] = {
+    
+    guard let arrows = UIImage(named: "plantuml-sequence-arrows")?.extractTiles( with: CGSize( width: 158.0, height: 28.6) ) else {
+        return [ [], [] ,[], [] ]
+    }
+
+    return [ [], [], arrows, [] ]
+}()
+
+
+// [StackOverflow](https://stackoverflow.com/a/73628496/521197)
+extension UIImage {
+
+    func extractTiles(with tileSize: CGSize) -> [UIImage?] {
+        
+        let hCount = Int(self.size.height / tileSize.height )
+        let wCount = Int(self.size.width / tileSize.width )
+
+        var tiles:[UIImage] = []
+
+        for i in 0...hCount-1 {
+            for p in 0...wCount-1 {
+                let rect = CGRect(
+                    x: CGFloat(p) * tileSize.width,
+                    y: CGFloat(i) * tileSize.height,
+                    width: tileSize.width,
+                    height: tileSize.height)
+                let temp:CGImage = self.cgImage!.cropping(to: rect)!
+                tiles.append(UIImage(cgImage: temp))
+            }
+        }
+        return tiles
+    }
+    
+}
 
 public struct PlantUMLKeyboardView: View {
         
@@ -78,24 +142,24 @@ public struct PlantUMLKeyboardView: View {
             
             ScrollView(.vertical, showsIndicators: false) {
                 
-                VStack(spacing: 5){
+                VStack(spacing: 15){
                     
-                    ForEach(plantUMLSymbols,id: \.self) { i in
+                    ForEach( Array(plantUMLSymbols.enumerated()), id: \.element) { rowIndex, i in
                         
-                        HStack(spacing: 10){
+                        HStack(spacing: 10) {
                             
-                            ForEach(i,id: \.self) { symbol in
+                            ForEach( Array(i.enumerated()), id: \.element ) { cellIndex, symbol in
                                 
-                                Button(action: {
+                                Button {
                                     
-                                    self.value = symbol
+                                    replaceSymbolAtCursorPosition(symbol)
                                     
-                                    print( "TextViewResponder \(String(describing: getFirstTextFieldResponder()))")
-                                }) {
+                                } label: {
                                     
-                                    Text(symbol)
-                                        .font(.system(size: 15))
+                                    ButtonLabel( rowIndex: rowIndex, cellIndex: cellIndex, symbol: symbol )
+                                    
                                 }
+                                .buttonStyle( KeyButtonStyle() )
                             }
                         }
                     }
@@ -118,8 +182,52 @@ public struct PlantUMLKeyboardView: View {
         }
     }
     
+    //
+    func replaceSymbolAtCursorPosition( _ symbol: String) {
+        guard let handleToYourTextView = getFirstTextFieldResponder() else {
+            return
+        }
+        
+        print( "TextViewResponder \(handleToYourTextView)")
+        
+        // [How to programmatically enter text in UITextView at the current cursor position](https://stackoverflow.com/a/35888634/521197)
+        if let range = handleToYourTextView.selectedTextRange {
+            // From your question I assume that you do not want to replace a selection, only insert some text where the cursor is.
+              handleToYourTextView.replace(range, withText: symbol)
+        }
+
+    }
 }
 
+struct KeyButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(3)
+            .border( .black, width: 1)
+    }
+}
+
+
+extension PlantUMLKeyboardView {
+    
+    func ButtonLabel( rowIndex: Int, cellIndex: Int, symbol: String ) -> some View  {
+        
+        Group {
+            if plantUMLImages[rowIndex].isEmpty || plantUMLImages[rowIndex].isEmpty ||   plantUMLImages[rowIndex][cellIndex]==nil
+            {
+                Text(symbol)
+                    .font(.system(size: 16).bold())
+
+            }
+            else {
+                let img = plantUMLImages[rowIndex][cellIndex]
+                Image( uiImage: img! )
+                    .resizable()
+                    .frame(width: 40, height: 20)
+            }
+        }
+    }
+}
 
 struct PlantUMLKeyboardView_Previews: PreviewProvider {
     static var previews: some View {
