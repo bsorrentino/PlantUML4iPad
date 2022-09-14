b5e076ac096286d81363cd25ee676743df49d4e3
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-09-14 12:46:18 +0200

    chore: update project settings

--- SwiftPlantUML.playground/Contents.swift
+++ SwiftPlantUML.playground/Contents.swift
@@ -3,7 +3,30 @@ import PlantUMLFramework
 
 var greeting = "Hello, playground"
 
-let clazz = SyntaxStructure( rawValue: "class Test" )
+let text = "/‘ of participant '/"
+
+var result = text
+
+text.enumerated().forEach { index, ch in
+    let v = ch.unicodeScalars.first!
+    print( v, v.value )
+    if( !ch.isASCII ) {
+        if v.value == 8216 {
+            var chars = Array(text)
+            chars[index] = "\u{27}" // 39
+            result = String( chars )
+        }
+    }
+//    print( "\($0)" )
+//    if( !$0.isASCII ) {
+//        let v = $0.unicodeScalars.first!
+//        print( v, v.value )
+//    }
+//    else {
+//        print( "\($0.asciiValue!)" )
+//    }
+}
+let clazz = SyntaxStructure( rawValue: result )
 
 let script = PlantUMLScript( items: [clazz] )
 
