ba1f587445564eca5b960b1751e652cb1424fd5d
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-09-18 17:37:06 +0200

    chore: remove deps

--- PlantUMLKeyboard/Package.swift
+++ PlantUMLKeyboard/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", branch: "master" ),
+        // .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", branch: "master" ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
