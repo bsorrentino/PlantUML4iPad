eb1fd39ee76e06281356a114292f99d145453d95
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-03-11 20:35:13 +0100

    feat(PlantUMLKeyboard+Choices.swift): setup Choice Key Button class
    #15

---
+++ PlantUMLKeyboard/Sources/PlantUMLKeyboard/PlantUMLKeyboard+Choices.swift
@@ -0,0 +1,20 @@
+//
+//  PlantUMLKeyboard+Choices.swift
+//  
+//
+//  Created by Bartolomeo Sorrentino on 11/03/23.
+//
+
+import SwiftUI
+
+struct ChoicesKeyButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChoicesKeyButton()
+    }
+}
