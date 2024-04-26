ff8bea19356071eb9d514b0d6d2a4497529ab0a0
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-04-26 14:33:39 +0200

    build: update LangGraph

--- AIAgent/Package.swift
+++ AIAgent/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["AIAgent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/bsorrentino/LangGraph-Swift.git", exact: "1.2.1"),
+        .package(url: "https://github.com/bsorrentino/LangGraph-Swift.git", exact: "1.2.2"),
 //        .package(path: "/Users/bsorrentino/WORKSPACES/GITHUB.me/AppleOS/LangGraph-Swift"),
         .package(url: "https://github.com/bsorrentino/Swift-OpenAI.git", branch: "develop"), // Add the dependency here
         ],
