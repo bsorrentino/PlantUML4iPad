96208ccb1ba80515492feb564071e87d7eac7a83
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-05-14 20:06:47 +0200

    refactor(AppSecureStorage): update property access modifiers

--- AppSecureStorage/Sources/AppSecureStorage/KeychainWrapper.swift
+++ AppSecureStorage/Sources/AppSecureStorage/KeychainWrapper.swift
@@ -47,10 +47,10 @@ open class KeychainWrapper {
     public static let standard = KeychainWrapper()
     
     /// ServiceName is used for the kSecAttrService property to uniquely identify this keychain accessor. If no service name is specified, KeychainWrapper will default to using the bundleIdentifier.
-    private (set) public var serviceName: String
+    private(set) public var serviceName: String
     
     /// AccessGroup is used for the kSecAttrAccessGroup property to identify which Keychain Access Group this entry belongs to. This allows you to use the KeychainWrapper with shared keychain access between different applications.
-    private (set) public var accessGroup: String?
+    private(set) public var accessGroup: String?
     
     private static let defaultServiceName: String = {
         return Bundle.main.bundleIdentifier ?? "SwiftKeychainWrapper"
