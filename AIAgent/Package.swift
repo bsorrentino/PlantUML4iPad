9f1366b9cc0beab66de2f7e3f738d1da944a19dc
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-12-11 19:38:36 +0100

    refactor: move to minimum support iOS 17.
    - Add AnyLanguageModel dependency

--- AIAgent/Package.swift
+++ AIAgent/Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "AIAgent",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v13)
+        .iOS(.v17),
+        .macOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -17,11 +17,13 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "https://github.com/bsorrentino/LangGraph-Swift.git", exact: "2.0.0"),
-        .package(url: "https://github.com/bsorrentino/LangGraph-Swift.git", branch: "main"),
 //        .package(url: "https://github.com/bsorrentino/Swift-OpenAI.git", branch: "develop"), // Add the dependency here
 //        .package(url: "https://github.com/MacPaw/OpenAI.git", branch: "main")
-        .package(url: "https://github.com/MacPaw/OpenAI.git", exact: "0.4.3"),
 //        .package(url: "https://github.com/1amageek/OpenFoundationModels-OpenAI.git", branch: "main")
+//        .package(url: "https://github.com/mattt/AnyLanguageModel", from: "0.5.0")
+        .package(url: "https://github.com/bsorrentino/LangGraph-Swift.git", branch: "main"),
+        .package(url: "https://github.com/MacPaw/OpenAI.git", exact: "0.4.3"),
+        .package(url: "https://github.com/bsorrentino/AnyLanguageModel.git", branch: "develop")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -31,7 +33,8 @@ let package = Package(
             dependencies: [
 //                .product(name: "OpenAI", package: "Swift-OpenAI"),
                 .product(name: "OpenAI", package: "OpenAI"),
-                .product(name: "LangGraph", package: "LangGraph-Swift")
+                .product(name: "LangGraph", package: "LangGraph-Swift"),
+                .product(name: "AnyLanguageModel", package: "AnyLanguageModel")
             ], resources: [ .process("Resources")]),
         .testTarget(
             name: "AIAgentTests",
--- PlantUML/FoundationModel/Prompting.swift
+++ PlantUML/FoundationModel/Prompting.swift
@@ -5,21 +5,32 @@
 //  Created by bsorrentino on 06/11/25.
 //
 import Playgrounds
-import FoundationModels
-//import OpenFoundationModels
+import AnyLanguageModel
 
 @Generable
-struct StructturedOutput {
-    
+struct Output {
+
+    @Guide(description: "affermative or negative response")
+    var answer: String
 }
 
 
 #Playground {
-    let session = LanguageModelSession()
     
-    let sl = SystemLanguageModel.default.supportedLanguages
-    
-    let response = try await session.respond(to: "do you know plantuml script language?")
 
+    if let apiKey = readConfigString(forInfoDictionaryKey: "OPENAI_API_KEY"), !apiKey.isEmpty {
+        
+        let model = OpenAILanguageModel(
+            apiKey: apiKey,
+            model: "gpt-4o-mini"
+        )
+        
+        let session = LanguageModelSession( model: model )
+        
+        let response = try await session.respond( to: "do you know plantuml script language?", generating: Output.self)
+        
+        let answer = response.content.answer
+        
+    }
 }
 
