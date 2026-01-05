dd597490c29ec5e5e35dadc968c93c76889a951d
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-01-05 12:15:31 +0100

    refactor(PlantUMLDocument): add backgroundImage in saving process

--- PlantUML/PlantUMLDocument.swift
+++ PlantUML/PlantUMLDocument.swift
@@ -17,10 +17,12 @@ extension UTType {
 fileprivate struct Content {
     let text: String
     let drawing: Data?
-
+    let backgroundImage: UIImage?
+    
     private enum CodingKeys: String, CodingKey {
         case text
-        case drawing // = "image" // Optional name change for clarity (optional)
+        case drawing
+        case backgroundImage
     }
 
 }
@@ -31,35 +33,47 @@ extension Content : Encodable {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         
-        
-//        guard let textUTF8 = text.data(using: .utf8) else {
-//            throw Errors.documentEncodeError("Cannot convert text in utf8")
-//        }
-        
         try container.encode(text, forKey: .text)
+        
         if let drawing {
             try container.encode(drawing.base64EncodedString(), forKey: .drawing)
         }
-//        try container.encodeIfPresent(drawing, forKey: .drawing)
+        
+        if let backgroundImage, let pngData = backgroundImage.pngData() {
+            try container.encode(pngData.base64EncodedString(), forKey: .backgroundImage)
+        }
     }
 
 }
 
 // Decodable Extension
 extension Content : Decodable {
+    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        
         text = try container.decode(String.self, forKey: .text)
 
         if let base64String = try container.decodeIfPresent(String.self, forKey: .drawing) {
             if let data = Data(base64Encoded: base64String) {
                 drawing = data
             } else {
-                throw Errors.documentDecodeError("Invalid base64 string")
+                throw Errors.documentDecodeError("Invalid base64 string for drawing")
             }
         } else {
             drawing = nil
         }
+        
+        if let base64String = try container.decodeIfPresent(String.self, forKey: .backgroundImage) {
+            if let data = Data(base64Encoded: base64String) {
+                backgroundImage = UIImage(data: data)
+            } else {
+                throw Errors.documentDecodeError("Invalid base64 string for background image")
+            }
+        } else {
+            backgroundImage = nil
+        }
+
     }
 }
 
@@ -68,6 +82,7 @@ struct PlantUMLDocument: FileDocument {
 
     var text: String
     var drawing: Data?
+    var backgroundImage: UIImage?
     
     var isNew:Bool {
         text.isEmpty
@@ -91,6 +106,7 @@ struct PlantUMLDocument: FileDocument {
         
             text = content.text
             drawing = content.drawing
+            backgroundImage = content.backgroundImage
         }
         catch { // Backward compatibility
             if let string = String(data: data, encoding: .utf8) { // Backward compatibility
@@ -102,7 +118,7 @@ struct PlantUMLDocument: FileDocument {
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
         
         let encoder = JSONEncoder()
-        let content = Content(text: text, drawing: drawing)
+        let content = Content(text: text, drawing: drawing, backgroundImage: backgroundImage)
         let data = try encoder.encode(content)
         return .init(regularFileWithContents: data)
     }
